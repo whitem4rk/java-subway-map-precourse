31a8978aaa0797db009a3f61694983380dcf6ae9
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-13 19:19:12 +0900

    feat: Created MainView(view)

---
+++ src/main/java/subway/View/MainView.java
@@ -0,0 +1,15 @@
+package subway.View;
+
+public class MainView {
+
+    static final String CHOOSE = "## 원하는 기능을 선택하세요";
+    static final String LINEFEED = "\n";
+
+    private static final String MAIN = "## 메인 화면\n" +
+            "1. 역 관리\n" +
+            "2. 노선 관리\n" +
+            "3. 구간 관리\n" +
+            "4. 지하철 노선도 출력\n" +
+            "Q. 종료";
+
+}
