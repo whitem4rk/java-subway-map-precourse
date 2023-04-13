cf929af3682f88df6733b82415268152e58d7b04
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-13 19:16:49 +0900

    feat: Created StationView(view)

---
+++ src/main/java/subway/View/StationView.java
@@ -0,0 +1,34 @@
+package subway.View;
+
+import java.util.List;
+
+public class StationView {
+
+    private static final String STATION = "## 역 관리 화면\n" +
+                                            "1. 역 등록\n" +
+                                            "2. 역 삭제\n" +
+                                            "3. 역 조회\n" +
+                                            "B. 돌아가기";
+
+    private static final String ENROLL = "## 등록할 역 이름을 입력하세요.";
+    private static final String DELETE = "## 삭제할 역 이름을 입력하세요.";
+    private static final String LIST = "## 역 목록";
+
+    public static void printStation() {
+        System.out.println(STATION);
+        System.out.println(MainView.LINEFEED);
+        System.out.println(MainView.CHOOSE);
+    }
+
+    public static void printEnroll() {
+        System.out.println(ENROLL);
+    }
+
+    public static void printDelete() {
+        System.out.println(DELETE);
+    }
+
+    public static void printList() {
+        System.out.println(LIST);
+    }
+}
