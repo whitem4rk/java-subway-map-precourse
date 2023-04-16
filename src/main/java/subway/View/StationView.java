e7b8342f10fcfa89e9dcab265ddc38d6ef7c0bb6
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-16 20:57:01 +0900

    feat: Made StationView(view) include line feed

--- src/main/java/subway/View/StationView.java
+++ src/main/java/subway/View/StationView.java
@@ -4,19 +4,18 @@ import java.util.List;
 
 public class StationView {
 
-    private static final String STATION = "## 역 관리 화면\n" +
+    private static final String STATION = "\n## 역 관리 화면\n" +
                                             "1. 역 등록\n" +
                                             "2. 역 삭제\n" +
                                             "3. 역 조회\n" +
                                             "B. 돌아가기";
 
-    private static final String ENROLL = "## 등록할 역 이름을 입력하세요.";
-    private static final String DELETE = "## 삭제할 역 이름을 입력하세요.";
-    private static final String LIST = "## 역 목록";
+    private static final String ENROLL = "\n## 등록할 역 이름을 입력하세요.";
+    private static final String DELETE = "\n## 삭제할 역 이름을 입력하세요.";
+    private static final String LIST = "\n## 역 목록";
 
     public static void printStation() {
         System.out.println(STATION);
-        System.out.println(MainView.LINEFEED);
         System.out.println(MainView.CHOOSE);
     }
 
