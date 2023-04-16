54b0cb25c8d4ea8e7fc5f835e0fe22a4ee6c822b
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-16 20:56:35 +0900

    feat: Made MainView(view) include line feed and printMain

--- src/main/java/subway/View/MainView.java
+++ src/main/java/subway/View/MainView.java
@@ -1,15 +1,21 @@
 package subway.View;
 
+import subway.domain.Line;
+
 public class MainView {
 
-    static final String CHOOSE = "## 원하는 기능을 선택하세요";
+    static final String CHOOSE = "\n## 원하는 기능을 선택하세요";
     static final String LINEFEED = "\n";
-
     private static final String MAIN = "## 메인 화면\n" +
             "1. 역 관리\n" +
             "2. 노선 관리\n" +
             "3. 구간 관리\n" +
             "4. 지하철 노선도 출력\n" +
             "Q. 종료";
+
+    public static void printMain() {
+        System.out.println(MAIN);
+        System.out.println(CHOOSE);
+    }
 
 }
