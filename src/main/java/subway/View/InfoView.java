3cea558d45de69f4c0e7a4763cadf6359a54d6e8
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-16 20:55:50 +0900

    feat: Made InfoView(view) include line feed

--- src/main/java/subway/View/InfoView.java
+++ src/main/java/subway/View/InfoView.java
@@ -1,22 +1,22 @@
 package subway.View;
 
 public class InfoView {
-    private static final String INFO = "[INFO] ";
+    public static final String INFO = "[INFO] ";
 
-    public static final String STATIONENROLLSUCCESS = INFO + "지하철 역이 등록되었습니다.";
-    public static final String STATIONDELETESUCCESS = INFO + "지하철 역이 삭제되었습니다.";
+    public static final String STATIONENROLLSUCCESS = "\n" + INFO + "지하철 역이 등록되었습니다.";
+    public static final String STATIONDELETESUCCESS = "\n" + INFO + "지하철 역이 삭제되었습니다.";
     public static void stationList() {
         // [INFO] XX역
     }
     // 두개 합칠순없을까
-    public static final String ROUTEENROLLSUCCESS = INFO + "지하철 노선이 등록되었습니다.";
-    public static final String ROUTEDELETESUCCESS = INFO + "지하철 노선이 삭제되었습니다.";
+    public static final String ROUTEENROLLSUCCESS = "\n" + INFO + "지하철 노선이 등록되었습니다.";
+    public static final String ROUTEDELETESUCCESS = "\n" + INFO + "지하철 노선이 삭제되었습니다.";
     public static void routeList() {
         // [INFO] X호선
     }
 
-    public static final String INTERVALENROLLSUCCESS = INFO + "구간이 등록되었습니다.";
-    public static final String INTERVALDELETESUCCESS = INFO + "구간이 삭제되었습니다.";
+    public static final String INTERVALENROLLSUCCESS = "\n" + INFO + "구간이 등록되었습니다.";
+    public static final String INTERVALDELETESUCCESS = "\n" + INFO + "구간이 삭제되었습니다.";
 
     public static void mapList() {
         // [INFO] X호선
